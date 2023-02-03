444c20281fbcbe5ae99e4dce36af6bc67a458180
redkite <julange@posteo.de>
2023-02-03 23:31:13 +0100

    Added planet range rings
    CHANGES:
    - added RangeCircle class to display range of a planet

--- Assets/Scripts/SpaceObject.cs
+++ Assets/Scripts/SpaceObject.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class SpaceObject : MonoBehaviour
 {
     [SerializeField] public float mass = 1f;
+    [SerializeField] public float range = 1f;
+
+    [SerializeField] private RangeCircle circle;
 
     private void Awake()
     {
+        circle = GetComponentInChildren<RangeCircle>();
         transform.localScale = new Vector3(mass, mass, mass);
     }
 
