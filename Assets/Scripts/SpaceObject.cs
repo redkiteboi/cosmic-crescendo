780801434ad15c9f155ffba90e9d16f9781ef52d
redkite <julange@posteo.de>
2023-02-03 23:44:54 +0100

    Reworked range circle
    CHANGES:
    - range circle is now part of the space object instad of a child game object

--- Assets/Scripts/SpaceObject.cs
+++ Assets/Scripts/SpaceObject.cs
@@ -7,17 +7,26 @@ public class SpaceObject : MonoBehaviour
     [SerializeField] public float mass = 1f;
     [SerializeField] public float range = 1f;
 
-    [SerializeField] private RangeCircle circle;
-
-    private void Awake()
-    {
-        circle = GetComponentInChildren<RangeCircle>();
-        transform.localScale = new Vector3(mass, mass, mass);
-    }
+    [SerializeField] private new LineRenderer renderer;
 
     private void OnValidate()
     {
         transform.localScale = new Vector3(mass, mass, mass);
+        DrawCircle(100, mass * range);
+    }
+
+    private void DrawCircle(int corners, float radius)
+    {
+        renderer.positionCount = corners;
+        for (int i = 0; i < corners; i++)
+        {
+            float circumference = (float)i / corners;
+            float radian = circumference * 2 * Mathf.PI;
+            float x = Mathf.Cos(radian) * radius;
+            float z = Mathf.Sin(radian) * radius;
+            Vector3 pos = new Vector3(x, 0, z);
+            renderer.SetPosition(i, pos);
+        }
     }
 
 }
